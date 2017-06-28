6447eaf41f2aedcdc9a59884a0cacbfb3cfcb81e
Patrick Mans <p.mans@rely.nl>
2017-06-28 11:02:57 +0200

    Rename vpc

--- Networking.tf
+++ Networking.tf
@@ -7,19 +7,14 @@
 #  Outline: creates VPC with two subnets (public/private)
 #
 ############################################################################
-module "vpc" {
+module "shcc" {
   source = "github.com/terraform-community-modules/tf_aws_vpc"
-
   name = "${var.vpc_name}"
-
   cidr = "${var.vpc_cidr}"
   private_subnets = "${var.vpc_private_subnet}"
   public_subnets  = "${var.vpc_public_subnet}"
-
   enable_nat_gateway = "${var.vpc_nat_enable}"
-
-  azs      = "${var.vpc_zones}"
-
+  azs = "${var.vpc_zones}"
   tags {
     "environment" = "${lookup(var.tags,"gen_tag_environment")}"
     "customer" = "${lookup(var.tags,"gen_tag_customer")}"
